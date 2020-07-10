31be6831a5fad9b54121ff1da9415ecba84bace5
PRAHLAD ANAND <prahlad.anand@salesforce.com>
2020-07-10 11:25:28 -0700

    fix port bind

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const port = 5000
 
 app.get('/', (req, res) => res.send("HELLO WORLD!"));
 
-app.listen(port, () => {
+app.listen((process.env.PORT || port), () => {
     console.log("listening....")
 })
